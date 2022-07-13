63963a754c496c5a258005d62d472613321cec7a
yueyuxin <yyue1@ualberta.ca>
2022-07-14 04:49:09 +0900

    add supervised training

--- mytrain2.py
+++ mytrain2.py
@@ -24,174 +24,11 @@ from imagecorruptions import corrupt, get_corruption_names
 from arch_unet import UNet
 from validate import validate, ValDatasetFile
 from utils import Generator, DUMP_IMAGES, calculate_psnr
+from mytrain import TrainDatasetCOCOOffline, TrainDatasetCOCOOnline, checkpoint
 
 import logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s-%(filename)s#%(lineno)d:%(message)s')
 logger = logging.getLogger('global')
-
-def checkpoint(net, epoch, name, save_model_path):
-    os.makedirs(save_model_path, exist_ok=True)
-    model_name = 'epoch_{}_{:03d}.pth'.format(name, epoch)
-    save_model_path = os.path.join(save_model_path, model_name)
-    torch.save(net.state_dict(), save_model_path)
-    logger.info('Checkpoint saved to {}'.format(save_model_path))
-
-
-def space_to_depth(x, block_size):
-    n, c, h, w = x.size()
-    unfolded_x = torch.nn.functional.unfold(x, block_size, stride=block_size)
-    return unfolded_x.view(n, c * block_size**2, h // block_size,
-                           w // block_size)
-
-
-def generate_mask_pair(img):
-    # prepare masks (N x C x H/2 x W/2)
-    n, c, h, w = img.shape
-    mask1 = torch.zeros(size=(n * h // 2 * w // 2 * 4, ),
-                        dtype=torch.bool,
-                        device=img.device)
-    mask2 = torch.zeros(size=(n * h // 2 * w // 2 * 4, ),
-                        dtype=torch.bool,
-                        device=img.device)
-    # prepare random mask pairs
-    idx_pair = torch.tensor(
-        [[0, 1], [0, 2], [1, 3], [2, 3], [1, 0], [2, 0], [3, 1], [3, 2]],
-        dtype=torch.int64,
-        device=img.device)
-    rd_idx = torch.zeros(size=(n * h // 2 * w // 2, ),
-                         dtype=torch.int64,
-                         device=img.device)
-    torch.randint(low=0,
-                  high=8,
-                  size=(n * h // 2 * w // 2, ),
-                  #generator=Generator.get_generator(), # debug
-                  out=rd_idx)
-    rd_pair_idx = idx_pair[rd_idx]
-    rd_pair_idx += torch.arange(start=0,
-                                end=n * h // 2 * w // 2 * 4,
-                                step=4,
-                                dtype=torch.int64,
-                                device=img.device).reshape(-1, 1)
-    # get masks
-    mask1[rd_pair_idx[:, 0]] = 1
-    mask2[rd_pair_idx[:, 1]] = 1
-    return mask1, mask2
-
-
-def generate_subimages(img, mask):
-    n, c, h, w = img.shape
-    subimage = torch.zeros(n,
-                           c,
-                           h // 2,
-                           w // 2,
-                           dtype=img.dtype,
-                           layout=img.layout,
-                           device=img.device)
-    # per channel
-    for i in range(c):
-        img_per_channel = space_to_depth(img[:, i:i + 1, :, :], block_size=2)
-        img_per_channel = img_per_channel.permute(0, 2, 3, 1).reshape(-1)
-        subimage[:, i:i + 1, :, :] = img_per_channel[mask].reshape(
-            n, h // 2, w // 2, 1).permute(0, 3, 1, 2)
-    return subimage
-
-
-class TrainDatasetCOCOOffline(Dataset):
-    def __init__(self, data_root, clean_root, corrupted_root, ann_file, patch=256, resize=True):
-        super(TrainDatasetCOCOOffline, self).__init__()
-        self.patch = patch
-
-        self.data_root = data_root
-        self.clean_root = clean_root
-        self.corrupted_root = corrupted_root
-
-        self.img_paths = [_['file_name'] for _ in json.load(open(ann_file))['images']]
-
-        self.transform = transforms.ToTensor()
-
-        self.resize_range = [256,512] if resize else None
-
-    def _get_param_crop(self, img):
-        w, h = F.get_image_size(img)
-        th, tw = self.patch, self.patch
-        if h + 1 < th or w + 1 < tw:
-            raise ValueError(f"Required crop size {(th, tw)} is larger then input image size {(h, w)}")
-
-        if w == tw and h == th:
-            return 0, 0, h, w
-
-        i = torch.randint(0, h - th + 1, size=(1,)).item()
-        j = torch.randint(0, w - tw + 1, size=(1,)).item()
-        return i, j, th, tw
-
-    def _get_resize_params(self, img):
-        w, h = F.get_image_size(img)
-        s, l = min(w,h), max(w,h)
-
-        if self.resize_range is None:
-            if s < self.patch:
-                return self.patch
-            return None
-
-        lb = int(self.resize_range[0]/s*l)
-        if lb <= self.resize_range[0]:
-            return self.resize_range[0]
-        return random.randint(self.resize_range[0], lb)
-
-    def _get_img(self, p):
-        image = Image.open(p)
-        if image.mode != 'RGB':
-            image = image.convert("RGB")
-        return image    
-
-    def __getitem__(self, idx):
-        img_path_clean = os.path.join(self.data_root, self.clean_root, self.img_paths[idx]) 
-        img_path_corrupted = os.path.join(self.data_root, self.corrupted_root, self.img_paths[idx]) 
-        imgcl = self._get_img(img_path_clean)
-        imgcr = self._get_img(img_path_corrupted)
-        assert imgcl.size == imgcr.size, f'{imgcl.size} {imgcr.size}'
-        return self._transform_img(imgcl, imgcr)
-
-    def _transform_img(self, imgcl, imgcr):
-        # resize
-        s = self._get_resize_params(imgcl)
-        if s is not None:
-            imgcl = F.resize(imgcl, s, F.InterpolationMode.BILINEAR, None, None)
-            imgcr = F.resize(imgcr, s, F.InterpolationMode.BILINEAR, None, None)
-
-        # random crop
-        i, j, h, w = self._get_param_crop(imgcl)
-        imgcl = F.crop(imgcl, i, j, h, w)
-        imgcr = F.crop(imgcr, i, j, h, w)
-        imgcl = self.transform(imgcl)
-        imgcr = self.transform(imgcr)
-        assert imgcl.shape[0] == 3, f'{img_path_clean}'
-        return imgcr, imgcl
-
-    def __len__(self):
-        return len(self.img_paths)
-
-class TrainDatasetCOCOOnline(TrainDatasetCOCOOffline):
-    def __init__(self, data_root, ann_file, corruption, patch=256, fix_random_seed=True, resize=True):
-        super(TrainDatasetCOCOOnline, self).__init__(data_root, None, None, ann_file, patch, resize)
-        self.corruption = corruption
-        self.severity = 3
-        self.fix_random_seed = fix_random_seed
-
-    def __getitem__(self, idx):
-        img_path_clean = os.path.join(self.data_root, self.img_paths[idx])
-        imgcl = self._get_img(img_path_clean)
-        # get_corruption
-        if self.fix_random_seed:
-            np.random.seed(idx)
-        if self.corruption == 'random':
-            corruption = random.choice(get_corruption_names())
-        else:
-            corruption = self.corruption
-        arraycr = corrupt(np.array(imgcl), corruption_name=corruption, severity=self.severity)
-        imgcr = Image.fromarray(arraycr)
-        return self._transform_img(imgcl, imgcr)
-
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--data_root', type=str)
@@ -219,10 +56,7 @@ if __name__ == '__main__':
     parser.add_argument('--n_val', type=int, default=None)
     parser.add_argument('--batchsize', type=int, default=4)
     parser.add_argument('--patchsize', type=int, default=256)
-    parser.add_argument("--Lambda1", type=float, default=1.0)
-    parser.add_argument("--Lambda2", type=float, default=1.0)
-    parser.add_argument("--Lambda3", type=float, default=0.0)
-    parser.add_argument("--increase_ratio", type=float, default=2.0)
+    parser.add_argument("--Lambda", type=float, default=1.0)
 
     opt, _ = parser.parse_known_args()
     opt.dump_images = DUMP_IMAGES[opt.dump_images]
@@ -304,15 +138,14 @@ if __name__ == '__main__':
             noisy = noisy.cuda()
 
             optimizer.zero_grad()
+            
 
+            """
             mask1, mask2 = generate_mask_pair(noisy)
             noisy_sub1 = generate_subimages(noisy, mask1)
             noisy_sub2 = generate_subimages(noisy, mask2)
-            if opt.Lambda3 > 0:
+            with torch.no_grad():
                 noisy_denoised = network(noisy)
-            else:
-                with torch.no_grad():
-                    noisy_denoised = network(noisy)
             noisy_sub1_denoised = generate_subimages(noisy_denoised, mask1)
             noisy_sub2_denoised = generate_subimages(noisy_denoised, mask2)
 
@@ -324,21 +157,20 @@ if __name__ == '__main__':
 
             loss1 = torch.mean(diff**2)
             loss2 = Lambda * torch.mean((diff - exp_diff)**2)
-            if opt.Lambda3 > 0:
-                loss3 = torch.mean(torch.square(noisy_denoised-clean))
-                loss_all = opt.Lambda1 * loss1 + opt.Lambda2 * loss2 + opt.Lambda3 * opt.increase_ratio * loss3
-            else:
-                loss_all = opt.Lambda1 * loss1 + opt.Lambda2 * loss2
+            loss_all = opt.Lambda1 * loss1 + opt.Lambda2 * loss2
+            """
+
+            output = network(noisy)
+            loss_all = opt.Lambda*torch.mean(torch.square(output-clean))
 
             loss_all.backward()
             optimizer.step()
             if iteration % opt.log_freq == 0:
-                psnr = calculate_psnr(noisy_denoised.detach().cpu().numpy(), clean.cpu().numpy())
+                psnr = calculate_psnr(output.detach().cpu().numpy(), clean.cpu().numpy())
                 logger.info(
-                        '{:04d} {:05d} Loss1={:.6f}, Lambda={}, Loss2={:.6f}, Loss3={:.6f}, Loss_Full={:.6f}, Time={:.4f}'
-                .format(epoch, iteration, np.mean(loss1.item()), Lambda,
-                        np.mean(loss2.item()), np.mean(loss3.item()), np.mean(loss_all.item()),
-                        time.time() - st))
+                        '{:04d} {:05d} Loss_Full={:.6f}, Time={:.4f} psnr={:.2f}'
+                .format(epoch, iteration, loss_all.item(),
+                        time.time() - st, psnr))
 
         scheduler.step()
 
