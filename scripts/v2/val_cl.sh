d6a290684bef73399f26adafef8aacf60f7820cb
yueyuxin <yyue1@ualberta.ca>
2022-10-06 02:19:47 +0900

    modify val_cl

--- scripts/v2/val_cl.sh
+++ scripts/v2/val_cl.sh
@@ -1,4 +1,4 @@
-exp=1
+
 
 
 
@@ -10,12 +10,12 @@ export PYTHONPATH=$PYTHONPATH:$ROOT
 python $ROOT/validate.py \
 --save_model_path=./results_clean/epoch${e} \
 --val_dir=/yueyuxin/data/coco/val2017 \
+--val_ann_file=/yueyuxin/mmdetection/corruption_benchmarks/${p}/${m}/${c}-3/failure_annotation_fltest_${exp}.json \
 --dump_images=DENOISED_ONLY \
 --ckpt=results/train/model_best.pth \
 --gpu=$1 \
 2>&1|tee valcl.log
 
-#--val_ann_file=/yueyuxin/mmdetection/corruption_benchmarks/retinanet/retinanet_r50_fpn_1x_coco/gaussian_noise-3/failure_annotation_fltrain_2.json \
 #--ckpt=results/unet_gauss25_b4e100r02/2022-06-19-12-37/checkpoints/epoch_model_002.pth \
 #--ckpt=pretrained_model/model_gauss25_b4e100r02.pth \
 #--val_dir=validation/Kodak \
